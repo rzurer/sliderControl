909f9ef714c1d0dc92192a66ec985000151c79c3
Robert Zurer <robert@zurer.com>
2012-11-30 07:06:54 -0500

    removed common files

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 "use strict";
 var initialize = function () {
 		var common, slider, identifiers;
-		common = require('./modules/common').common(localStorage);
+		common = require('../common/modules/common').common(localStorage);
         slider = require('./modules/slider').slider(common);
         window.slider = slider;
     };
